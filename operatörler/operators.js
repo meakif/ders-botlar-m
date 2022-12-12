ff5100cbbc8f06a7e1c7960dafa6afa34096c807
Akif <mehmet61875@gmail.com>
2022-12-12 22:02:57 +0300

    ikinci ders sonu

--- operatörler/operators.js
+++ operatörler/operators.js
@@ -36,3 +36,50 @@ console.log(isNaN(difference)) // nan mıdır? kontrolü yapar.
 const yearOfBirth = 2000
 const name = "Akif"
 console.log(name+ " is " + (2022 - yearOfBirth) +" years old.");
+
+//! Template literals
+
+console.log(`${name} is ${2022 - yearOfBirth} years old.`)
+
+
+// Carpma ve Us alma
+
+const pi = 3.14
+const r = Number(prompt("Please enter r"))
+console.log(r);
+const square = pi * r**2
+console.log(`Square of circle: ${square}`);
+
+//?Bazi fonksiyonlar
+// Math.floor();  //* her zaman en yakin alt tamsayiya yuvarlar
+// Math.ceil();  //* her zaman en yakin ust tam tamsayiya yuvarlar
+// Math.trunc(); //* sayinin tam kismini alir.
+// Math.round(); //* en yakin tam sayiya yuvarlar.
+// Math.random(); //* 0 ve 1 arasında rasgele sayi uretir.
+
+const randomNumber = Math.random()
+console.log(randomNumber);
+
+// 0 ile 100 arası rastgele tam sayı üretme
+
+const rastgelesayı = Math.round(Math.random() * 100) // maksimum 99.99 yapar.
+console.log(rastgelesayı);
+
+
+// arttırma ve azaltma (ınc dec)
+
+let a = 5
+a++
+console.log(a++); // 6
+y = a + 5 // 7 + 5
+console.log(--y); // 11
+
+let z = 4
+z = z + 5
+z += 10 // z = z + 10
+console.log(z);
+
+let k = 20
+k -= 10 
+console.log("k", --k);
+console.log({ k });
