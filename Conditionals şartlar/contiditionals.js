7060ccdaac35be30091e6850165b9b5ebae9f9e6
Akif <mehmet61875@gmail.com>
2022-12-14 21:25:34 +0300

    şartlar first

--- Conditionals şartlar/contiditionals.js
+++ Conditionals şartlar/contiditionals.js
@@ -3,3 +3,16 @@
 // * =======================================================
 
 console.log(" **** Conditions ****")
+
+
+// Consoledan girilen bir sayının pozitif negatif yada 0 olduğunu tespit ederek yazdiriniz
+
+const num = prompt("bir sayı giriniz")
+
+if(num > 0){
+    console.log(`${num} sayisi pozitiftir.`)
+}else if(num < 0){
+    console.log(`${num} sayisi negatiftir.`);
+}else{
+    console.log(`${num} sayisi 0 dir.` );
+}
