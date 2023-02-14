9023369bead842aaf0b67263644afe8efc29110b
Akif <mehmet61875@gmail.com>
2023-02-14 21:09:01 +0300

    react lesson 2-1

---
+++ react/ders2/rc02_jsx-comp-sytling/src/App.js
@@ -0,0 +1,15 @@
+import { Footer } from "./Footer"
+import Header from "./Header"
+
+//* App Componenti
+function App() {
+  return (
+    //* jsx
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default App
