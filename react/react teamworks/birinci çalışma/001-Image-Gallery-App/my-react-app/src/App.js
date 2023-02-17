ed4aeeea6f53f513b339210f6dbdc151d6add9ef
Akif <mehmet61875@gmail.com>
2023-02-17 23:21:07 +0300

    project solution

---
+++ react/react teamworks/birinci çalışma/001-Image-Gallery-App/my-react-app/src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/Header';
+import Main from './components/Main';
+import './styles/style.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ react/react teamworks/birinci çalışma/README.md
@@ -0,0 +1,8 @@
+# ReactJS Coding Challenges
+
+List of coding challenges within React workshops as follows;
+
+[01 - Image Gallery App ](./001-Image-Gallery-App/README.md)
+
+
+
