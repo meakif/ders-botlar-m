29249feb97eb559c33ba5c3859d32d10a3b6624c
Akif <mehmet61875@gmail.com>
2023-02-18 16:48:59 +0300

    Tour Places finish

--- react/react teamworks/tour-places/my-react-app/src/App.js
+++ react/react teamworks/tour-places/my-react-app/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Main from './components/Main/Main';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <Navbar />
       <h1>Tour Places</h1>
+      <Main />
     </div>
   );
 }
