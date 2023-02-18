6c5a82bbc25a066483dc8703a8bbc26571cdfdbe
Akif <mehmet61875@gmail.com>
2023-02-18 15:48:18 +0300

    Tour Places part1

---
+++ react/react teamworks/tour-places/my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Tour Places</h1>
+    </div>
+  );
+}
+
+export default App;
